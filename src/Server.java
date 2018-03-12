b693d15bad429a1ca24c4cdebcbc1a163a2deefe
Çağatay Sel <selcagatay1@hotmail.com>
2018-03-13 00:40:14 +0300

    Add files via upload

--- src/Server.java
+++ src/Server.java
@@ -11,57 +11,68 @@ package server;
  */
 import java.net.*;
 import java.io.*;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Server {
-    
+
     public static final String serverIpAccessPoint = "192.168.43.193";
-    public static final String serverIpBilkent = "139.179.225.19";
-    public static final int serverPort =54134;
+    public static final String serverIpBilkent = "139.179.200.208";
+    public static final int serverPort = 54134;
+    public static final AtomicInteger numConnectedClients = new AtomicInteger(0);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        int numOfClients = 0;
-        UserHandler[] userHandlers = new UserHandler[4];
+        int numTotalClients = 0;
         
+        List<UserHandler> userHandlers = Collections.synchronizedList(new ArrayList<UserHandler>());
+        
+        
+
         try {
 
             ServerSocket serverSocket = new ServerSocket(serverPort, 0, InetAddress.getByName(serverIpBilkent));
             int serverPort = serverSocket.getLocalPort();
             String serverAddress = serverSocket.getLocalSocketAddress().toString();
-            System.out.println("Server is running on port: "+serverPort +" and adress: "+serverAddress);
+            System.out.println("Server is running on port: " + serverPort + " and adress: " + serverAddress);
 
-            while (numOfClients != 4) {
+            while (true) {
                 Socket newClient = serverSocket.accept();
-                
+
                 System.out.println("Client connected.");
-                System.out.println("Address: "+newClient.getRemoteSocketAddress().toString()+" port: "+newClient.getLocalPort());
-                UserHandler userHandler = new UserHandler(newClient);
-                userHandlers[numOfClients] = userHandler;
-                numOfClients++;
-                 
-                
-                new Thread(
-                        userHandler).start();
-                
-                for(int i=0;i<numOfClients;i++){
-                 userHandlers[i].sendMessage("Merhaba client i:"+i);      
-                }
-                 
-                
-                
-            }            
+                System.out.println("Address: " + newClient.getRemoteSocketAddress().toString() + " port: " + newClient.getLocalPort());
+                UserHandler userHandler = new UserHandler(newClient, numTotalClients);
+                //userHandlers.add(userHandler);
+                numTotalClients++;
+
+                new Thread(userHandler).start();
+
+            }
 
         } catch (Exception e) {
             System.out.println("bura hata verdi");
             System.out.println(e.getMessage());
             e.printStackTrace();
-            
-            
+
         } finally {
-            
+
         }
     }
+
+    public static void decreaseNumConnectedClients() {
+        numConnectedClients.getAndIncrement();
+
+    }
+
+    public static void increaseNumConnectedClients() {
+        numConnectedClients.getAndDecrement();
+
+    }
+    
+    
 }
--- src/UserHandler.java
+++ src/UserHandler.java
@@ -1,26 +1,23 @@
 package server;
 
 import java.net.*;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-
-
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class UserHandler implements Runnable {
 
     public final int INVALID_ID = -1;
     public final int FAILED_REQUEST = -2;
     public final int SUCCESFUL_REQUEST = -3;
-    
 
     //Request in which client does not expect to receive data
-    public final int REQUEST_REGISTER = 0;    
+    public final int REQUEST_REGISTER = 0;
     public final int REQUEST_ADD_NEW_USER_ITEM = 1;
     public final int REQUEST_ADD_NEW_PLAYED_DEBATE = 2;
     public final int REQUEST_ADD_NEW_PAST_DEBATE = 3;
-    public final int REQUEST_ENTER_LOBBY = 4;    
+    public final int REQUEST_ENTER_LOBBY = 4;
     public final int REQUEST_CHANGE_SELECTED_AVATAR = 5;
     public final int REQUEST_CHANGE_SELECTED_TITLE = 6;
     public final int REQUEST_CHANGE_SELECTED_FRAME = 7;
@@ -38,91 +35,115 @@ public class UserHandler implements Runnable {
     public final int FORWARD_PLAYED_DEBATES = 102;
     public final int FORWARD_PAST_DEBATES = 103;
     public final int FORWARD_BUYABLE_ITEMS = 104;
-    
-    
-    
 
     private Socket socket;
     private String clientAddress;
-    private boolean running;
-    private PrintWriter out;
-    private BufferedReader in;
 
-    public UserHandler(Socket socket)  {
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public UserHandler(Socket socket, int threadId) {
 
         this.socket = socket;
         this.clientAddress = socket.getRemoteSocketAddress().toString();
-        running = true;
-        
+
         try {
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        } 
-        catch(Exception e){
+
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+
+        } catch (Exception e) {
+
             System.out.println(e.getMessage());
             System.out.println("Failed to initiate input output streams");
-            
         }
 
     }
 
     public void run() {
 
-        //Try block with resources
-        //This block closes the resources when it is done.
-        try  {
-            String inputLine;
-            while (running) {
-                
-                inputLine = in.readLine();
-                
-                if (inputLine == null) {
-                    System.out.println("Client disconnected");
-                    return;
-                }
-                
-                /*boolean requestHandledSuccessfully =handleIncomingRequests(requestId,Object[] requestParameters);
-                if(requestHandledSuccessfully){
-                    System.out.println("Request handled successfully.");
-                }else{
-                    System.out.println("Request handle failed.");
-                }*/
-                
-            
-                
-                if (inputLine.equals("HELLO SERVER")) {
-                    out.println("HELLO CLIENT");
-                } else {
-                    out.println("Not hello");
-                }
+        try {
 
+            ArrayList<Object> requestParams = new ArrayList<Object>();
+
+            
+            boolean endOfStreamReached = false;
+            int requestId = in.readInt();
+            System.out.println("Request id: " + requestId);
+
+            while (!endOfStreamReached) {
+                try {
+                    Object curObject = in.readObject();
+                    if(curObject==null){
+                        endOfStreamReached = true;
+                    }else{
+                        requestParams.add(curObject);
+                    }
+                    
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                    endOfStreamReached = true;
+                }
             }
+            System.out.println("params size: "+requestParams.size());
+            for(int i=0;i<requestParams.size();i++){
+                System.out.println(requestParams.get(i).toString());
+            }
+
+            handleIncomingRequests(requestId, requestParams);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+                System.out.println("Client connection ended");
+            } catch (Exception ignored) {
+
+            }
+
         }
 
     }
-    
-    public synchronized void sendMessage(String message){
-        
-        if(out == null){
-                    System.out.println("out is null");
-                }
+
+    /*public synchronized void sendMessage(String message) {
+
+        if (out == null) {
+            System.out.println("out is null");
+        }
         out.println(message);
-                
         
+
+    }*/
+//Below method were not needed and they probably wont work as intented.
+/*public synchronized void terminate() {
+        listening = false;
+        try {
+            socket.close();
+
+        } catch (Exception e) {
+            System.out.println("Couldn't closed client socket");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+
+        }
     }
 
-    public void terminate() {
-        running = false;
+    public synchronized void pauseThread() {
+        listening = false;
     }
 
-    /*private boolean handleIncomingRequests(int requestId, Object[] requestParameters) {
+    public synchronized void continueThread() {
+        listening = true;
+    }*/
+    private boolean handleIncomingRequests(int requestId, ArrayList<Object> requestParams) {
 
         switch (requestId) {
 
             case (REQUEST_REGISTER):
+                handleRequestRegister(requestParams);
                 break;
             case (REQUEST_SIGN_IN):
                 break;
@@ -133,7 +154,7 @@ public class UserHandler implements Runnable {
             case (REQUEST_ADD_NEW_PAST_DEBATE):
                 break;
             case (REQUEST_ENTER_LOBBY):
-                
+
                 break;
             case (REQUEST_CHANGE_SELECTED_AVATAR):
                 break;
@@ -150,79 +171,103 @@ public class UserHandler implements Runnable {
             case (REQUEST_GET_BUYABLE_ITEMS):
                 break;
         }
-    }
-    
-    private boolean handleRequestRegister(){
         return false;
     }
-    private boolean handleRequestSignIn(){
+
+    private boolean handleRequestRegister(ArrayList<Object> requestParams) {
+        String data1 = "data1";
+        String data2 = "data2";
+        try {
+            out.writeObject(data1);
+            out.writeObject(data2);
+            out.writeObject(null);
+            out.flush();
+            System.out.println("objects send");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return false;
+        }
+
+    }
+    /*
+    private boolean handleRequestSignIn(ArrayList<Object> requestParams) {
         User user;
-        
+
         forwardUserObject(user);
         return false;
     }
-    private boolean handleRequestAddNewUserItem(){
+
+    private boolean handleRequestAddNewUserItem(ArrayList<Object> requestParams) {
         return false;
     }
-    private boolean handleRequestAddNewPlayedDebate(){
+
+    private boolean handleRequestAddNewPlayedDebate(ArrayList<Object> requestParams) {
+        DebateManager.addNewPlayedDebate(debate);
         return false;
     }
-    private boolean handleRequestAddNewPastDebate(){
+
+    private boolean handleRequestAddNewPastDebate(ArrayList<Object> requestParams) {
         return false;
     }
-    private boolean handleRequestEnterLooby(){
+
+    private boolean handleRequestEnterLooby(ArrayList<Object> requestParams) {
         return false;
     }
-    private boolean handleRequestChangeSelectedAvatar(){
+
+    private boolean handleRequestChangeSelectedAvatar(ArrayList<Object> requestParams) {
         return false;
     }
-    private boolean handleRequestChangeSelectedTitle(){
+
+    private boolean handleRequestChangeSelectedTitle(ArrayList<Object> requestParams) {
         return false;
     }
-    private boolean handleRequestChangeSelectedFrame(){
+
+    private boolean handleRequestChangeSelectedFrame(ArrayList<Object> requestParams) {
         return false;
     }
-    private boolean handleRequestGetInventory(){
+
+    private boolean handleRequestGetInventory(ArrayList<Object> requestParams) {
         Inventory inventory;
         forwardInventory(inventory);
         return false;
     }
-    private boolean handleRequestGetPlayedDebates(){
+
+    private boolean handleRequestGetPlayedDebates(ArrayList<Object> requestParams) {
+        Debate[] debates = DebateManager.getPlayaedDebates(userId);
+        return false;
+
+    }
+
+    private boolean handleRequestGetPastDebates(ArrayList<Object> requestParams) {
         Debate[] debates;
         return false;
-        
     }
-    private boolean handleRequestGetPastDebates(){
-        Debate[] debates;
-        return false;
-    }
-    private boolean handleRequestGetBuyableItems(){
+
+    private boolean handleRequestGetBuyableItems(ArrayList<Object> requestParams) {
         Item[] items;
         return false;
     }
-    
-    private boolean forwardUserObject(User user){
-        return false;
-    }
-    private boolean forwardInventory(Inventory inventory){
-        return false;
-    }
-    private boolean forwardPlayedDebates(Debate[] debates){
-        return false;
-    }
-    private boolean forwardPastDebates(Debate[] debates){
-        return false;
-    }
-    private boolean forwardBuyableItems(Item[] items){
-        
-        return false;
-    }
-    */
-    
-    
-     
-    
-    
-    
 
+    private boolean forwardUserObject(User user) {
+        return false;
+    }
+
+    private boolean forwardInventory(Inventory inventory) {
+        return false;
+    }
+
+    private boolean forwardPlayedDebates(Debate[] debates) {
+        return false;
+    }
+
+    private boolean forwardPastDebates(Debate[] debates) {
+        return false;
+    }
+
+    private boolean forwardBuyableItems(Item[] items) {
+
+        return false;
+    }
+     */
 }
