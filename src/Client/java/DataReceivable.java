b4aa5666a378d1f0e2e541ad543bb3d6d73867ff
Çağatay Sel <selcagatay1@hotmail.com>
2018-03-26 18:10:46 +0300

    Add files via upload

--- src/Client/java/DataReceivable.java
+++ src/Client/java/DataReceivable.java
@@ -4,11 +4,14 @@ package dicomp.debateit;
  * Created by Cagatay on 11.03.2018.
  */
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /**
  * UI classes who receive data from server and update their ui according to received data must should this interface.
  */
 public interface DataReceivable {
 
-    public boolean receiveAndUpdateUI(Object[] objects);
+    public boolean receiveAndUpdateUI(int responseId,ArrayList<Serializable> responseData);
     public void updateRetrieveProgress(int progress);
 }
