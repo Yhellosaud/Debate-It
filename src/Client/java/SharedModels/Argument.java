649377b721c6c24d864259b8c22eb09e39d68d9c
Çağatay Sel <selcagatay1@hotmail.com>
2018-04-15 14:05:04 +0300

    Add files via upload

---
+++ src/Client/java/SharedModels/Argument.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SharedModels;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Cagatay
+ */
+public class Argument implements Serializable{
+    
+    private static final long serialVersionUID = 4L;
+    
+    private int sentTime;
+    private int stage;
+    private String argument;
+    
+    public Argument(int sentTime,int stage,String argument){
+        
+        this.sentTime = sentTime ;
+        this.stage = stage;
+        this.argument = argument;
+        
+    }
+    
+}
