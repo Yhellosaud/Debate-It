d7831e5c44b5839897ac683d4b54f86e3326f6b2
Yasin Balcancı <36000428+ybalcanci@users.noreply.github.com>
2018-05-04 15:49:11 +0300

    Add files via upload

--- src/Client/java/SharedModels/Idea.java
+++ src/Client/java/SharedModels/Idea.java
@@ -1,6 +1,7 @@
 package SharedModels;
 
 import java.io.Serializable;
+import java.util.Random;
 
 /**
  * Created by Yasin on 11.03.2018.
@@ -13,11 +14,14 @@ public class Idea implements Serializable {
     public static final int CATEGORY_PHILOSOPHY =2;
     public static final int CATEGORY_HISTORY =3;
     public static final int CATEGORY_EDUCATION =4;
-    
-    
-    private static final long serialVersionUID = 3L;
-    
 
+
+    private static final long serialVersionUID = 3L;
+    private static final String[] statement0 = {"Alternative medicine is nothing but a lie.", "Marijuana should be legal."};
+    private static final String[] statement1 = {"Statism is a better economic model than Neoliberalism.", "Communism is more fair than Capitalism."};
+    private static final String[] statement2 = {"Humans have a good and a bad side inborn.", "There is life after death."};
+    private static final String[] statement3 = {"Sultan Abdulhamid II was a high-skilled leader.", "Russians have no important effect on history."};
+    private static final String[] statement4 = {"Private schools should not be allowed.", "Teachers should be tested regularly."};
 
     private int ideaID;
     private String statement;
@@ -33,6 +37,25 @@ public class Idea implements Serializable {
         this.ideaID = ideaID;
         this.category = category;
         this.statement = statement;
+    }
+
+    public static Idea generateIdea(int cat){
+        Random rand = new Random();
+        String s;
+        int i = rand.nextInt(statement0.length);
+        switch (cat){
+            case 0: s = statement0[i];
+                    break;
+            case 1: s = statement1[i];
+                    break;
+            case 2: s = statement2[i];
+                    break;
+            case 3: s = statement3[i];
+                    break;
+            default: s = statement4[i];
+                    break;
+        }
+        return new Idea(cat, s, cat);
     }
 
     public int getIdeaID() {
