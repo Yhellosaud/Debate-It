1754219540f956fe167b82edd13020ea5114313b
Yasin Balcancı <36000428+ybalcanci@users.noreply.github.com>
2018-05-05 12:41:49 +0300

    Add files via upload

--- src/Client/java/MarketActivity.java
+++ src/Client/java/MarketActivity.java
@@ -1,13 +1,44 @@
 package dicomp.debateit;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import SharedModels.*;
 
 public class MarketActivity extends AppCompatActivity {
-
+    MarketAdapter adaptor;
+    ListView listItems;
+    Button but;
+    ArrayList<Item> items;
+    ServerBridge sb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market);
+        but = (Button)findViewById(R.id.getItems);
+        listItems = (ListView)findViewById(R.id.itemList);
+        //listItems.setVisibility(View.GONE);
+
+        items = new ArrayList<Item>();
+        items.add(Title.getTitle(2));
+        items.add(new Avatar(100)); items.add(new Avatar(101)); items.add(new Avatar(102));
+        items.add(new Avatar(102)); items.add(Title.getTitle(0));
+        adaptor = new MarketAdapter(MarketActivity.this, items);
+        listItems.setAdapter(adaptor);
+        listItems.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?>adapter,View v, int position, long a){
+                Button item = (Button)adapter.getItemAtPosition(position);
+                
+            }
+        });
     }
 }
