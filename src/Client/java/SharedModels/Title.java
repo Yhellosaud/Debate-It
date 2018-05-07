0c79fb4fe8212598975ad7c4eaacd88eddda3444
Yasin Balcancı <36000428+ybalcanci@users.noreply.github.com>
2018-05-07 16:16:29 +0300

    Add files via upload

--- src/Client/java/SharedModels/Title.java
+++ src/Client/java/SharedModels/Title.java
@@ -7,12 +7,37 @@ public class Title extends Item implements Serializable {
 
     private static final long serialVersionUID = 7L;
 
-    int titleID;
-    String title;
+    private static final String[] titles = {"Master", "King", "Socrates", "Sarkozy"};
 
-    public Title(int titleID, String title) {
+    private int titleID;
+    private String titleName;
+
+    public Title(int titleID, String titleName) {
         super(titleID);
-        this.title = title;
+        this.titleName = titleName;
+    }
+
+    public int getTitleID() {
+        return titleID;
+    }
+
+    public String getTitleName() {
+        return titleName;
+    }
+
+    public static Title getTitle(int val){
+        switch (val){
+            case 0: return new Title(0, titles[0]);
+            case 1: return new Title(1, titles[1]);
+            case 2: return new Title(2, titles[2]);
+            default: return new Title(3, titles[3]);
+        }
+    }
+    public ArrayList<Title> getAllTitles(){
+        ArrayList<Title> titlesAL= new ArrayList<Title>();
+        for(int i = 0; i < titles.length; i++)
+            titlesAL.add(new Title(i, titles[i]));
+        return titlesAL;
     }
 
     public Title() {
@@ -22,11 +47,7 @@ public class Title extends Item implements Serializable {
     public String toString() {
         return super.toString() +
                 ", type= Title" +
-                ", title:" + title +
+                ", title:" + titleName +
                 '}';
-    }
-
-    public String getTitleString(){
-        return title;
     }
 }
