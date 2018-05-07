3a00f15250c18b04c0a9a98aebba73cc93c11036
Çağatay Sel <selcagatay1@hotmail.com>
2018-05-07 16:53:45 +0300

    Update Avatar.java

--- src/Client/java/SharedModels/Avatar.java
+++ src/Client/java/SharedModels/Avatar.java
@@ -7,20 +7,18 @@ public class Avatar extends Item implements Serializable {
 
     private static final long serialVersionUID = 6L;
 
-    int avatarID;
+
 
     public Avatar(int avatarID) {
         super(avatarID);
-        this.avatarID = avatarID;
     }
 
     public Avatar() {
         super();
-        avatarID = 0;
     }
 
     public int getAvatarID() {
-        return avatarID;
+        return super.getItemID();
     }
 
     public String toString() {
