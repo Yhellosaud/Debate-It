231792ab87734e6ad108a6ea9f62fc1d4f0db9fd
Yasin Balcancı <36000428+ybalcanci@users.noreply.github.com>
2018-05-07 16:11:47 +0300

    Add files via upload

---
+++ src/Client/java/Title.java
@@ -0,0 +1,53 @@
+package SharedModels;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class Title extends Item implements Serializable {
+
+    private static final long serialVersionUID = 7L;
+
+    private static final String[] titles = {"Master", "King", "Socrates", "Sarkozy"};
+
+    private int titleID;
+    private String title;
+
+    public Title(int titleID, String title) {
+        super(titleID);
+        this.title = title;
+    }
+
+    public int getTitleID() {
+        return titleID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static Title getTitle(int val){
+        switch (val){
+            case 0: return new Title(0, titles[0]);
+            case 1: return new Title(1, titles[1]);
+            case 2: return new Title(2, titles[2]);
+            default: return new Title(3, titles[3]);
+        }
+    }
+    public ArrayList<Title> getAllTitles(){
+        ArrayList<Title> titlesAL= new ArrayList<Title>();
+        for(int i = 0; i < titles.length; i++)
+            titlesAL.add(new Title(i, titles[i]));
+        return titlesAL;
+    }
+
+    public Title() {
+        super();
+    }
+
+    public String toString() {
+        return super.toString() +
+                ", type= Title" +
+                ", title:" + title +
+                '}';
+    }
+}
