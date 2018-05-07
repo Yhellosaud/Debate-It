b4d5992f3cbf05c098996c2a9c63022fa87dfcc6
Ahmet Sarıgüney <36050511+Yhellosaud@users.noreply.github.com>
2018-05-07 14:50:14 +0300

    Update UserManager.java

--- src/Database/UserManager.java
+++ src/Database/UserManager.java
@@ -5,12 +5,9 @@
  */
 package mysqlconnection;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Connection;
 import SharedModels.*;
 import java.util.*;
 
@@ -18,227 +15,87 @@ import java.util.*;
  *
  * @author Yhellosaud
  */
-public class UserManager {
-    
-    private Statement s;
-    private ResultSet rs;
-    private String userData;
-    private ResultSetMetaData rsmd;
-    private PreparedStatement statement;
-    private final Connection c;
-    private int numberOfUsers;
-    
-    public UserManager(Connection c) throws SQLException {
+public class MySqlConnection {
 
-        this.c = c;
-        s = c.createStatement();
-        c.setAutoCommit(true);
-        numberOfUsers = 0;
-    }
-    
-    public boolean signUp(String username, String password) throws SQLException {
-                
-        userData = "SELECT * FROM user";
-        s = c.createStatement();
-        rs = s.executeQuery(userData);
-        rsmd = rs.getMetaData();
+    public static void main(String[] args) {
         
-        int userID = 0;
+        String url = "jdbc:mysql://localhost:3306/didatabase?autoReconnect=true&useSSL=false";
+        String username = "root";
+        String password = "dicomp319";
+        Connection c;
+        System.out.println("Connecting database...");
         
-        while(rs.next()) {
-
-            if(username.equals(rs.getString(2)) || password.equals(rs.getString(3))) {
-                
-                return false;
-            }
+        try {
             
-            userID = rs.getInt(1);
-        }
-        
-        insertUser(new User(username, password, ++userID, null, null, null));
-        numberOfUsers++;
-        
-        return true;
-    }
-    
-    public User signIn(String username, String password) throws SQLException {
-        
-        userData = "SELECT * FROM user";
-        s = c.createStatement();
-        rs = s.executeQuery(userData);
-        rsmd = rs.getMetaData();
-        
-        int userID = 0;
-        
-        while(rs.next()) {
-
-            if(username.equals(rs.getString(2)) && password.equals(rs.getString(3))) {
-                
-                
-                return getUserDetails(username);
-            }
-        }
-
-        return null;
-    }
-    
-    public boolean setSelectedAvatar(String username, Avatar selectedAvatar) throws SQLException {
-        
-        
-        userData = "SELECT * FROM user";
-        s = c.createStatement();
-        rs = s.executeQuery(userData);
-        rsmd = rs.getMetaData();
-        
-        int userID = 0;
-        
-        while(rs.next()) {
-
-            if(username.equals(rs.getString(2))) {
-
-                statement.setInt(4, selectedAvatar.getAvatarID());
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
-    
-    
-    public boolean checkUserName(String username) {
-        
-    
-        return true;
-    }
-    
-    public int getNumberOfUsers() {
-        
-        return numberOfUsers;
-    }
-    
-    public User getUserDetails(String username) throws SQLException {
-        
-        int userID = 0;
-        String password = " ";
-        Avatar selectedAvatar = null;
-        ArrayList<Integer> playedDebateIDs = new ArrayList<Integer>();
-        ArrayList<Integer> votedDebateIDs = new ArrayList<Integer>();
-        
-        userData = "SELECT * FROM user";
-        s = c.createStatement();
-        rs = s.executeQuery(userData);
-        rsmd = rs.getMetaData();
-
-        while (rs.next()) {
-            //Table display
-            for(int i = 1; i <= rsmd.getColumnCount(); i++) {
-
-                if (i > 1) { 
-                    System.out.print(",  ");
-                }
-
-                System.out.print(rsmd.getColumnName(i) + ": " + rs.getString(i));
-            }
-            System.out.println("");
-            //Searched user data is found and retrieved respectively
-            if(rs.getString(2).equals(username)) {
-
-                userID = rs.getInt(1);
-                password = rs.getString(3);
-                selectedAvatar = new Avatar(rs.getInt(4));
-            }
-        }
+            c = (Connection)DriverManager.getConnection(url, username, password); 
+            System.out.println("Database connected!\n");
+            //////////////////////////////////////////////////////////////////////////
+            ////////////////////////TEST CASE FOR USER MANAGER////////////////////////
+            //////////////////////////////////////////////////////////////////////////
+            UserManager um = new UserManager(c);
+            ArrayList<Integer> playedDebateIDs = new ArrayList<>(Arrays.asList(3, 5, 8, 17, 1, 2));
+            ArrayList<Integer> votedDebateIDs = new ArrayList<>(Arrays.asList(31, 71, 6, 10, 14));
             
-        userData = "SELECT * FROM user_playeddebateid";
-        rs = s.executeQuery(userData);
-        rsmd = rs.getMetaData();
-
-        String[] dids = new String[20];
-
-        while(rs.next()) {
-
-            System.out.print(",  ");
-            System.out.println(rsmd.getColumnName(2) + " " + rs.getString(2));
-            String ids = rs.getString(2);
-            dids = ids.split(",");
-        }
-
-        for(int i = 0; i < dids.length; i++) {
-
-            int id = Integer.parseInt(dids[i]);
-            playedDebateIDs.add(i, id);
-
-        }
-
-        userData = "SELECT * FROM user_voteddebateid";
-        rs = s.executeQuery(userData);
-        rsmd = rs.getMetaData();
-
-        while(rs.next()) {
-
-            System.out.print(",  ");
-            System.out.println(rsmd.getColumnName(2) + " " + rs.getString(2));
-            String ids = rs.getString(2);
-            dids = ids.split(",");
-        }
-
-        for(int i = 0; i < dids.length; i++) {
-
-            int id = Integer.parseInt(dids[i]);
-            votedDebateIDs.add(i, id);
-
-        }
-        
-        return (new User(username, password, userID, playedDebateIDs, votedDebateIDs, selectedAvatar));
-    }
-    
-    public void insertUser(User user) throws SQLException {
-        
-        //Each single information data of user is attained one by one
-        int userID = user.getUserID();
-        String username = user.getUsername();
-        String password = user.getPassword();
-        int avatarID = user.getSelectedAvatar().getAvatarID();
-        ArrayList<Integer> pastDebateIDs = user.getPlayedDebateIDs();
-        ArrayList<Integer> votedDebateIDs = user.getVotedDebateIDs();
-        
-        String pdids = "";
-        String vdids = "";
-        
-        for(int i = 0; i < pastDebateIDs.size(); i++) {
+            um.signUp("Eda Hanım", "141511");
+            um.signUp("Ayşe Çavuş", "401230");
+            um.signUp("Mehmet Öğretmen", "22234");
+            um.signUp("Ali Kasap", "88adaj21");
+            um.signUp("Komşu Hüseyin", "bentekim68");
             
-            pdids += "" + pastDebateIDs.get(i) + ",";
+            System.out.println();
+            
+            um.addPlayedDebateID("Eda Hanım", 15);
+            um.addPlayedDebateID("Eda Hanım", 40);
+            um.addPlayedDebateID("Eda Hanım", 20);
+            um.addPlayedDebateID("Ayşe Çavuş", 112);
+            um.addPlayedDebateID("Ayşe Çavuş", 5);
+            um.addPlayedDebateID("Ayşe Çavuş", 62);
+            um.addPlayedDebateID("Ayşe Çavuş", 8);
+            um.addPlayedDebateID("Ayşe Çavuş", 41);
+            um.addPlayedDebateID("Komşu Hüseyin", 7);
+            um.addPlayedDebateID("Komşu Hüseyin", 4);
+              
+            um.addVotedDebateID("Eda Hanım", 9);
+            um.addVotedDebateID("Eda Hanım", 18);
+            um.addVotedDebateID("Eda Hanım", 27);
+            um.addVotedDebateID("Eda Hanım", 36);
+            um.addVotedDebateID("Eda Hanım", 45);
+            um.addVotedDebateID("Ali Kasap", 6);
+            um.addVotedDebateID("Ali Kasap", 12);
+            um.addVotedDebateID("Ali Kasap", 19);
+            um.addVotedDebateID("Ali Kasap", 34);
+            um.addVotedDebateID("Ali Kasap", 23);
+            um.addVotedDebateID("Ali Kasap", 117);
+            um.addVotedDebateID("Ali Kasap", 5);
+            
+            System.out.println();
+            
+            um.displayAllUsers();
+            
+            System.out.println("\nNUMBER OF USERS: " + um.getNumberOfUsers());
+            //////////////////////////////////////////////////////////////////////////
+            ////////////////////////TEST CASE FOR ITEM MANAGER////////////////////////
+            //////////////////////////////////////////////////////////////////////////
+            
+            
+            //////////////////////////////////////////////////////////////////////////
+            ////////////////////////TEST CASE FOR DEBATE MANAGER//////////////////////
+            //////////////////////////////////////////////////////////////////////////
+            /*DebateManager dm = new DebateManager(c);
+            ArrayList<Player> players = new ArrayList<>();
+            Idea idea = new Idea(1, "Anam bacım kardeşim midir", 8);
+            int[] debateIDs = {3, 13, 31, 193, 131, 40};
+            Debate debate = new Debate(idea, players, 1, 80, 1, 3, 110, 90, 35, 40);
+            dm.getUserDebates(debateIDs);*/
+            //////////////////////////////////////////////////////////////////////////
+            ////////////////////////TEST CASE FOR DEBATE_MANAGER////////////////////////
+            //////////////////////////////////////////////////////////////////////////
         }
-        for(int i = 0; i < votedDebateIDs.size(); i++) {
         
-            vdids += "" + votedDebateIDs.get(i) + ",";
+        catch (SQLException e) {
+            
+            System.out.println("Cannot connect the database!");
+            e.printStackTrace();
         }
-        
-        //Related database tables are prepared in order to perform insertion
-        //The table is transfered into a prepared statement
-        //Statement variants are loaded with user information
-        userData = "INSERT INTO user (userID, username, password) VALUES (?, ?, ?)";
-        statement = c.prepareStatement(userData); 
-        statement.setInt(1, userID);
-        statement.setString(2, username);
-        statement.setString(3, password);
-        statement.setInt(4, avatarID);
-        statement.executeBatch();
-        statement.executeUpdate();
-        //////////////////////////////////////////////////////////////////////
-        userData = "INSERT INTO user_playeddebateid (userID, debateID) VALUES (?, ?)";
-        statement = c.prepareStatement(userData); 
-        statement.setInt(1, userID);
-        statement.setString(2, pdids);
-        statement.executeBatch();
-        statement.executeUpdate();
-        //////////////////////////////////////////////////////////////////////
-        userData = "INSERT INTO user_voteddebateid (userID, debateID) VALUES (?, ?)";
-        statement = c.prepareStatement(userData);
-        statement.setInt(1, userID);
-        statement.setString(2, vdids);
-        statement.executeBatch();
-        statement.executeUpdate();
     }
 }
